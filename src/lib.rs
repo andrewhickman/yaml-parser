2b299638a88596527136ee487ec477a2cf2d6f11
Andrew Hickman <andrew.hickman1@sky.com>
2024-01-22 20:00:38 +0000

    Sketch API

---
+++ src/lib.rs
@@ -0,0 +1,65 @@
+//! A pure-rust, safe, YAMl parser.
+#![no_std]
+#![warn(missing_debug_implementations, missing_docs)]
+#![deny(unsafe_code)]
+#![doc(html_root_url = "https://docs.rs/yaml-parser/0.1.0/")]
+
+/// A handler for events and tokens in a YAML stream.
+pub trait Receiver {
+    /// Called on each event in the YAML stream.
+    fn event(&mut self, event: Event, span: Span);
+
+    /// Called for every token of the input stream. The stream may be reconstructed by joining the returned tokens.
+    fn token(&mut self, token: Token, span: Span) {
+        let _ = (token, span);
+    }
+}
+
+/// A high level event in a YAML stream.
+#[derive(Copy, Clone, Debug)]
+pub enum Event {
+    /// Emitted at the start of parsing a YAML stream.
+    StreamStart {},
+    /// Emitted at the end of parsing a YAML stream.
+    StreamEnd {},
+    /// Emitted at the start of each document within a YAML stream.
+    DocumentStart {},
+    /// Emitted at the end of each document within a YAML stream.
+    DocumentEnd {},
+    /// Emitted at the start of a mapping node.
+    MappingStart {},
+    /// Emitted at the end of a mapping node.
+    MappingEnd {},
+    /// Emitted at the start of a sequence node.
+    SequenceStart {},
+    /// Emitted at the end of a sequence node.
+    SequenceEnd {},
+    /// Emitted when encountering an alias node.
+    Alias {},
+    /// Emitted when encountering a scalar node.
+    Scalar {},
+}
+
+/// A token type in a YAML stream.
+#[derive(Copy, Clone, Debug)]
+pub enum Token {}
+
+/// A range of characters within a source file.
+#[derive(Copy, Clone, Debug, Hash, Eq, PartialEq)]
+pub struct Span {
+    /// The start of the span (inclusive).
+    pub start: usize,
+    /// The end of the span (exclusive).
+    pub end: usize,
+}
+
+/// The position of a character in a source file.
+#[derive(Copy, Clone, Debug, Hash, Eq, PartialEq)]
+pub struct Location {
+    /// The byte offset of the character.
+    pub index: usize,
+    /// The 0-based line number of the character.
+    pub line: usize,
+    /// The byte offset of the character within the line.
+    pub column: usize,
+}
